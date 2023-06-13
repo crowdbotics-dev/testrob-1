5640837d0b9c4c84191a1fa686e2e52a339121fe
Robert So <robkso@gmail.com>
2023-06-13 10:00:42 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "testrob-1",
   url: "https://testrob-1.botics.co",
   api: "https://testrob-1.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,28 @@
+import { Text } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Text style={styles.QVATLsmB}>Lorem ipsum…</Text></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  QVATLsmB: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default Untitled1;
